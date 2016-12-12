5dff57777775397499d94cb6c4088963733036be
dangerstephen <dangersteve1@gmail.com>
2016-12-12 15:12:19 -0800

    updating to most recent

--- exercises/myFind.js
+++ exercises/myFind.js
@@ -1,8 +1,12 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Find
 function myFind(arr, callback) {
 
-//  CODE INSIDE HERE   //
+    for (i = 0; i < arr.length; i++) {
 
+        if (callback(arr[i], i, arr)) {
+            return arr[i];
+        }
+    }
 }
 
 /*
